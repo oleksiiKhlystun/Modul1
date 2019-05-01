9e5c2613e33ba16fa024370ab2ca32c745cd8772
Oleksii <48730885+oleksiiKhlystun@users.noreply.github.com>
2019-05-01 21:25:21 +0300

    changes about ReadLine to Main
    changes about Convert.ToInt32(Console.ReadLine()); to static void Main(string[] args)

--- Task_2_2a_Cons_Dr_Methods/Program.cs
+++ Task_2_2a_Cons_Dr_Methods/Program.cs
@@ -12,8 +12,12 @@ namespace Task_2_2a_Cons_Dr_Methods
                 //Create Box class instance
                 Console.WriteLine("Enter Wigth,Height,Simbol,Left,Top");
                 Box box = new Box();
+                box.Width = Convert.ToInt32(Console.ReadLine());
+				box.Height = Convert.ToInt32(Console.ReadLine());
+				box.Simbol = Convert.ToString(Console.ReadLine());
+				box.Left = Convert.ToInt32(Console.ReadLine());
+			    box.Top = Convert.ToInt32(Console.ReadLine());
                 //Use  Box.Draw() method
-
                 box.Draw();
                 Console.WriteLine("Press any key...");
                 Console.ReadLine();
